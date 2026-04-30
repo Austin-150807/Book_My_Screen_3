519dfffb85984454d53e6943edd2d7db3a0148dd
Mikey815 <ashm7112006@gmail.com>
2026-04-30 16:21:06 +0530

    final commit

--- bms-backend/src/config/config.ts
+++ bms-backend/src/config/config.ts
@@ -6,7 +6,7 @@ const _config = {
   databaseUrl: process.env.MONGO_CONNECTION_STRING,
   accessTokenSecret: process.env.ACCESS_TOKEN_SECRET as string,
   refreshTokenSecret: process.env.REFRESH_TOKEN_SECRET as string,
-  hashingSecret: process.env.HASH_SECRET as string,
+  hashingSecret: process.env.HASHING_SECRET as string,
   emailUsername: process.env.EMAIL_USERNAME as string,
   emailPassword: process.env.EMAIL_PASSWORD as string,
 };
